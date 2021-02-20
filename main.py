02011796db5bee0a49065199b8df5de90064b715
nmparfait <mbamu.developer@gmail.com>
2021-02-20 20:49:12 +0000

    screen sizes setup

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+
+#to help to track the time
+clock = pygame.time.Clock()
+
+FPS = 30
+
+#the color
+
+blue = (80,184,231)
+
+#screen settings
+screen_height = 700
+screen_width = 900
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#getting the screen sizes
+square_height, square_width = 140
+
+
+
+
+
+
+
+
+
+
+
+
+
